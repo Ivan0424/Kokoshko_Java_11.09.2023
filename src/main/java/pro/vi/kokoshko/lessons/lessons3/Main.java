80ab5b93243d0249553eba5bdac93a99344659e1
Kokoshko Ivan <vaniaivanov921@gmail.com>
2023-09-11 17:25:29 +0300

    lesson3

--- src/main/java/pro/vi/kokoshko/lessons/lessons3/Main.java
+++ src/main/java/pro/vi/kokoshko/lessons/lessons3/Main.java
@@ -1,4 +1,9 @@
 package pro.vi.kokoshko.lessons.lessons3;
 
 public class Main {
+    public static void main(String[] args) {
+       int a = 2;
+       int volume = 8*8 + 8*8 + 8*8;
+        System.out.println(a * volume);
+    }
 }
